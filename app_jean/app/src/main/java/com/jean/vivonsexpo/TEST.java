b7dbbb0a715b35790161ee385871e7a6a88c4843
jean <jean.mazagot78@gmail.com>
2021-03-17 09:45:59 +0100

    test commit

---
+++ app_jean/app/src/main/java/com/jean/vivonsexpo/TEST.java
@@ -0,0 +1,14 @@
+package com.jean.vivonsexpo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TEST extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t_e_s_t);
+    }
+}
