f8aefed0f8046b854a1f17e456f335ac17bd3f56
MAZAGOT Jean <JMazagot@alliance.local>
2021-06-24 15:36:46 +0200

    Forcer les variable de post

--- www_jean/authentification.php
+++ www_jean/authentification.php
@@ -3,7 +3,7 @@ require_once 'Param.php';
 require_once 'DBConnex.php';
 include_once 'userDAO.php';
 
-// $_POST['login'] = "jmazagot";
-// $_POST['mdp'] = "jmazagot";
+ $_POST['login'] = "jmazagot";
+ $_POST['mdp'] = "jmazagot";
 
 print(json_encode(userDAO::authentification($_POST['login'], $_POST['mdp'])));
